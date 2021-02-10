6dd24fd0e0a87939713353fdbc2d05bb06e567e6
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2021-02-10 17:15:30 +0100

    commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,6 @@ package org.virtuslab.inkuieexample
 class MyClass {
     fun main(args: Array<String>) {
         println("Hello World!")
-        1.toString()
     }
 
     fun muFunction(i: Int): Int = 1
